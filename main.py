b59b52a0c9e232e19a8a629e80c56e7c308482c3
Unknown <andreych2016@yandex.ru>
2018-07-27 11:46:13 -1200

    Hello Andy!

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from flask import Flaskapp = Flask(__name__)@app.route('/')def hello_world():    return 'Hello Tori!'if __name__ == '__main__':    app.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello Andy!'
+
+
+if __name__ == '__main__':
+    app.run()
