c6b2a54906f52ad15ea18d3dd5ecef2d80db5e0d
Nick Avdeev <avdeev@etlgr.com>
2018-07-27 18:28:59 +0500

    связь!

--- cli.py
+++ cli.py
@@ -1,1 +1,1 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-import socketsock = socket.socket()sock.connect(('127.0.0.1', 9090))sock.send('lol'.encode())data = sock.recv(1024)sock.close()# data = sock.recv(1024).decode()print(data)
+#!/usr/bin/env python# -*- coding: utf-8 -*-import socketsock = socket.socket()sock.bind(('192.168.3.156', 6000))sock.listen(1)while True:    conn, addr = sock.accept()    data = conn.recv(1024)    print(data)    conn.close()
--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-<<<<<<< Updated upstream
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello Andy!'
-
-
-if __name__ == '__main__':
-    app.run()
-=======
-from flask import Flaskimport requestsapp = Flask(__name__)@app.route('/')def hello_world():    requests.post(url='http://127.0.0.1:5000', data=None)    return 'Hello Tori!'if __name__ == '__main__':    app.run("0.0.0.0", port=22000)
->>>>>>> Stashed changes
+from flask import Flaskimport requestsapp = Flask(__name__)@app.route('/')def hello_world():    requests.post(url='http://127.0.0.1:5000', data=None)    return 'Hello Tori!'if __name__ == '__main__':    app.run("0.0.0.0", port=22000)
